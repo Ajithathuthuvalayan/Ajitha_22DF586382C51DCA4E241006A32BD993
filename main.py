142863959cdca8cf3829cd6d463c9fc25b171a8b
Ajithathuthuvalayan <144329937+Ajithathuthuvalayan@users.noreply.github.com>
2023-09-14 12:06:58 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-def fact_rec(n):
-  if n==0 or n==1:
-    return 1
-  else:
-    return n* fact_rec(n-1)
-number=2
-res=fact_rec(number)
-print("The factorial of {} is {}.".format(number,res))
+class player:
+  def play(self):
+    print('The player is playing circket')
+class Batsman(player):
+  def play(self):
+    print('The batsman is batting')
+class Bowler(player):
+  def play(self):
+    print('The bowler is bowling')
+p=player()
+p.play()
+B=Batsman()
+B.play()
+b=Bowler()
+b.play()
